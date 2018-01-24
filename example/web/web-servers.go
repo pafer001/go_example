f749097da346099ef65b7bca8d5fa0255ebc6804
pafer <zhenyawang@sohu-inc.com>
2018-01-24 17:30:41 +0800

    web images example

---
+++ example/web/web-servers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+)
+
+type Hello struct {
+
+}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+
+	fmt.Println(w, "hello")
+}
+
+
+
+func main()  {
+	var h Hello
+	err := http.ListenAndServe("localhost:4000", h)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
